1aeef30ff968348aa6b15a18b5498c470b16cacb
Victor-Dorel Mocanu <victor-dorel.mocanu@ikor.de>
2021-11-10 09:03:35 +0100

    edit logging

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,4 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "This is a chookity testing message... POOOK!"
-echo "This was added"
+echo "This was added then removed"
